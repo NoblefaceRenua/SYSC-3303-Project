7ac900219c3057d7c28fa0767b9755c48cb7c576
ultraodi <odinamclint@gmail.com>
2025-02-21 06:38:59 -0500

    Commit 6

---
+++ src/DroneSystemTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DroneSystemTest {
+    private Scheduler scheduler;
+    private DroneSystem droneSystem;
+    private int id;
+    private enum droneStatus {EMPTY, FULL, CRASH, ARRIVED};
+
+    @BeforeEach
+    void setUp() {
+        this.scheduler = new Scheduler();
+        this.id = 5;
+        droneSystem = new DroneSystem(scheduler, id);
+
+        new Thread(droneSystem).start();
+    }
+
+    @Test
+    void testRefillTank() {
+        droneSystem.refillTank();
+        assertTrue(droneSystem.getWaterLevel() == 100);
+    }
+
+    @Test
+    void testPour() {
+        droneSystem.pour();
+        assertFalse(droneSystem.getArrivalSensor());
+        assertFalse(droneSystem.getNozzle());
+        assertTrue(droneSystem.getTravel_seconds_spent() == 0);
+    }
+
+    @Test
+    void testMakeUnavailable() {
+
+        assertTrue(droneSystem.makeUnavailable());
+    }
+
+
+}
