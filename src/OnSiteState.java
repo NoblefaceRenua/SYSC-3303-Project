faaa0e7df28aa920edc6c1ec2d4aa0d55e3ee552
Clinton Odinamadu <odinamclint@gmail.com>
2025-03-16 00:04:07 -0400

    Update

--- src/OnSiteState.java
+++ src/OnSiteState.java
@@ -11,7 +11,7 @@ public class OnSiteState implements DroneState{
      */
     @Override
     public void handleStateChanged(DroneSystem drone) {
-        System.out.println("Drone " + drone.getId() + ": Arrived at fire incident");
+        System.out.println("[Drone " + drone.getId() + "]: Arrived at fire incident");
         drone.pour();
 //        drone.setState("Not Ready");
     }
